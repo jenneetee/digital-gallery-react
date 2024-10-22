8088736ef4945652b39cb3b329b102812631d2c0
jenneetee <jellytea284@gmail.com>
2024-10-21 23:39:55 -0400

    changed App.tsx to have links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import Message from './Message';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import ListGroup from './components/ListGroup';
 import Payments from './Payments';
 
 function App() {
-  return <div><Payments/></div>;
-  return <div> <Login/></div>;
-  return <div> <ListGroup/>  </div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            {/* Links to different pages */}
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/listgroup">List of Art</Link>
+            </li>
+            <li>
+              <Link to="/payments">Payments</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes to different components based on the URL */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/listgroup" element={<ListGroup />} />
+          <Route path="/payments" element={<Payments />} />
+          {/* Default route */}
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App; 
+export default App;
