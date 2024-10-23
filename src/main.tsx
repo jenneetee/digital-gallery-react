cf8c2aa0d83b9351b5f1f5a4a880514daffa3929
jenneetee <jellytea284@gmail.com>
2024-10-22 21:50:29 -0400

    changes gh pages to default to login.tsx page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import Router and Routes
+import App from './App';
+import Login from './Login';  // Import your Login component
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />  {/* Set the default route to Login */}
+        <Route path="/app" element={<App />} />  {/* Add any other routes you need */}
+        {/* Add more routes as necessary */}
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
